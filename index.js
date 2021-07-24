75a1fdda68df8f2a321d3d556f8f9a891be0e3ab
Maikell <maikell.ods10@gmail.com>
2021-07-24 00:13:11 -0400

    REST API Movie Lists CRD

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ dotenv.config();
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const movieRoute = require("./routes/movies");
+const listsRoute = require("./routes/lists");
 require("./database");
 
 // Middlewares
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/movies", movieRoute);
+app.use("/api/lists", listsRoute);
 
 app.listen(8800, () => {
     console.log("Backend server is running");
