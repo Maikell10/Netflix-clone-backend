a531431e6a8388eb35f9341442a72186b9c8d63c
Maikell <maikell.ods10@gmail.com>
2021-07-23 20:45:13 -0400

    REST API Users CRUD

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 require("./database");
 
 // Middlewares
@@ -12,6 +13,7 @@ app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 app.listen(8800, () => {
     console.log("Backend server is running");
